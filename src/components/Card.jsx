45f9318c8dcb7a98b16a8283cb3a3f31dbafe333
Srijan <srijanm20@gmail.com>
2024-07-29 13:27:38 +0545

    minor changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Card = ({ title, value,unit }) => {
   return (
-    <div className='col-5'>
+    <div className='col'>
       <div className='card-container bg-white border w-75 rounded p-3'>
         <p className='fs-6 fw-light'>{title}</p>
         <p className='fs-2 fw-bold mt-5'>{value}<span className='fs-5 fw-normal'>{unit}</span></p>
